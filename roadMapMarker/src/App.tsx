6ec40e58f27bf869e7ba79d4b7dfbfa19c386244
sehyun lee <mh080411@kyonggi.ac.kr>
2025-05-28 11:13:15 +0900

    최최종

--- roadMapMarker/src/App.tsx
+++ roadMapMarker/src/App.tsx
@@ -2,26 +2,6 @@ import './App.css'
 import MapPage from './components/mapPage/mapPage'
 
 function App() {
-
-  // const mapRef = useRef<HTMLDivElement | null>(null);
-
-  // useEffect(() => {
-  //   if (window.kakao && window.kakao.maps) {
-  //     window.kakao.maps.load(() => {
-  //       if (mapRef.current) {
-  //         const options = {
-  //           center: new window.kakao.maps.LatLng(37.5665, 126.9780),
-  //           level: 3,
-  //         };
-  //         const map = new window.kakao.maps.Map(mapRef.current, options);
-  //         mapRef.current = map;
-  //       }
-  //     });
-  //   }
-  // }, []);
-
-  // console.log('mapRef', mapRef);
-  // console.log('window.kakao', window.kakao);
   
 
 
